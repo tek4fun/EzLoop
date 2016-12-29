06389066db99be0ccd9b5f76b9f0531efdf2da85
iOS Student <iosstudent@techmasters-iMac.local>
2016-12-29 11:17:50 +0700

    update

--- EzLoop/ViewController.swift
+++ EzLoop/ViewController.swift
@@ -9,17 +9,56 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    var margin: CGFloat = 40
+    var n: Int = 0
+    
+    @IBOutlet weak var txt_balls: UITextField!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
     }
-
+    @IBAction func drawAction(_ sender: AnyObject) {
+        n = Int(txt_balls.text!)!;
+        drawBall()
+        
+    }
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+        
     }
-
-
+    func drawBall(){
+        
+        for subview in view.subviews {
+            if subview .isKind(of: UIImageView.self) {
+                subview.removeFromSuperview()
+            }
+        }
+        for iRow in 0..<n{
+            for iCollum in 0..<n {
+                let image = UIImage(named: "green")
+                let ball = UIImageView(image: image)
+                ball.center = CGPoint(x: margin+CGFloat(iRow) * widthBetweenBall(), y: CGFloat(iCollum)*heightBetweenBall() + margin+20)
+                self.view.addSubview(ball)
+            }
+        }
+    }
+    
+    
+    
+    func widthBetweenBall() ->CGFloat{
+        let space = (self.view.bounds.size.width - 2*margin)/CGFloat(n-1)
+        return space
+    }
+    func heightBetweenBall() -> CGFloat {
+        let space = (self.view.bounds.size.height - 2*margin)/CGFloat(n-1)
+        return space
+    }
+    
+    
 }
+
 
